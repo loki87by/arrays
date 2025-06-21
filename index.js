e6b1adb4d42516756ec62855eed4ed4affb92360
loki87by <loki87.666@gmail.com>
2025-06-22 00:29:55 +0300

    add repeater

--- index.js
+++ index.js
@@ -20,3 +20,55 @@ export const sum = (array, config = C.SUM_CONFIG) => {
   }
   return res;
 };
+
+export const repeater = (array1, array2, config = C.REPEATER_CONFIG) => {
+  const duplicatesArray = [];
+  const add = (e, i1, i2) => {
+    duplicatesArray.push({
+      element: e,
+      indexInFirstArray: i1,
+      indexInSecondArray: i2,
+    });
+  };
+  array1.forEach((i, index) => {
+    const primitive = ["string", "boolean", "number"].includes(typeof i);
+    const zero = i === null || i === undefined;
+    array2.forEach((j, ind) => {
+      if ((primitive || zero) && i === j) {
+        add(j, index, ind);
+      } else if (
+        i instanceof Date &&
+        j instanceof Date &&
+        Date.parse(new Date(i)) &&
+        Date.parse(new Date(j))
+      ) {
+        add(new Date(j), index, ind);
+      } else if (
+        i instanceof RegExp &&
+        j instanceof RegExp &&
+        i.source === j.source &&
+        i.flags === j.flags
+      ) {
+        add(new RegExp(j), index, ind);
+      } else if (
+        typeof i === "function" &&
+        typeof j === "function" &&
+        i === j
+      ) {
+        add(j, index, ind);
+      } else if (typeof i === "object" && typeof j === "object" && config) {
+        const comparedObj1 = config.fec ? config.fec(i) : i;
+        const comparedObj2 = config.sec ? config.sec(j) : j;
+        if (config.keys) {
+          const keys =
+            typeof config.keys === "string" && config.keys === "*"
+              ? Object.keys(comparedObj1)
+              : config.keys;
+          if (keys.every((k) => comparedObj1[k] === comparedObj2[k])) {
+            add(Object.assign({}, j), index, ind);
+          }
+        }
+      }
+    });
+  });
+};
--- src/consts.js
+++ src/consts.js
@@ -1,8 +1,15 @@
 export const SUM_CONFIG = {
-skipUndefined: false,
-skipNull: false,
-skipNaN: false,
-skipInfinity: false,
-booleanToNumber: false,
-stringToNumber: false,
+  skipUndefined: false,
+  skipNull: false,
+  skipNaN: false,
+  skipInfinity: false,
+  booleanToNumber: false,
+  stringToNumber: false,
+  callback: null
+};
+
+export const REPEATER_CONFIG = {
+    fec: null,  //firstElementCallback
+    sec: null,  //secondElementCallback
+    keys: null, // accepted '*' or string[]
 }
